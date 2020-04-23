8a84289b2e888d116bf1b22bbaf13f437587464d
Juan Jesús Padrón Hernández <jpadron@opencanarias.es>
2020-04-23 23:06:58 +0100

    Comprobación para responder solo a los tweets de la persona y no a los retweets

--- src/reply_bot.py
+++ src/reply_bot.py
@@ -3,23 +3,30 @@ from TwitterAPI import TwitterAPI
 from private import *
 
 TRACK_TERM = '759063401779920896'
-TWEET_TEXT = "@yisusisback Que te calles!"
-
+TWEET_TEXT = "@YisusIsBack Esto es una prueba"
 api = TwitterAPI(API_KEY, 
-                 API_KEY_SECRET,
-                 ACCESS_TOKEN,
-                 ACCESS_TOKEN_SECRET)
+                    API_KEY_SECRET,
+                    ACCESS_TOKEN,
+                    ACCESS_TOKEN_SECRET)
 
-r = api.request('statuses/filter', {'follow': TRACK_TERM})
+# TODO: Controlar que el texto del mensaje que se va a responder no es el mismo que 
+# TODO: el texto que se va a responder
+def reply_stream_tweets():
+    r = api.request('statuses/filter', {'follow': TRACK_TERM})
 
-for item in r:
-    print(item['id'] if 'id' in item else item)
+    for item in r:
+        print(item['text'] if 'text' in item else item)
 
-    if 'id' in item:
-        tweet_id = item['id']
+        print(item['user'])
 
-    print(item['text'] if 'text' in item else item)
+        print(item['user']['id'])
 
-    r = api.request('statuses/update', {'status': TWEET_TEXT, 'in_reply_to_status_id': tweet_id})
-    print('SUCCESS' if r.status_code == 200 else 'PROBLEM: ' + r.text)
+        if item['user']['id_str'] == TRACK_TERM :
+            if 'id' in item:
+                tweet_id = item['id']
 
+            print(item['text'] if 'text' in item else item)
+            t = api.request('statuses/update', {'status': TWEET_TEXT, 'in_reply_to_status_id': tweet_id})
+            print('SUCCESS' if t.status_code == 200 else 'PROBLEM: ' + t.text)
+
+reply_stream_tweets()
