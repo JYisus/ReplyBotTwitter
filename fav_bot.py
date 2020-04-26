eb2e70f50f08c62411ebab84881596a429fe94ae
Juan Jesús Padrón Hernández <jpadron@opencanarias.es>
2020-04-26 16:37:38 +0100

    Refactor reply_tweets

--- fav_bot.py
+++ fav_bot.py
@@ -15,25 +15,15 @@ api = TwitterAPI(API_KEY,
                     ACCESS_TOKEN,
                     ACCESS_TOKEN_SECRET)
 
-# TODO: Controlar que el texto del mensaje que se va a responder no es el mismo que 
-# TODO: el texto que se va a responder
-def reply_stream_tweets():
-    r = api.request('statuses/filter', {'follow': TRACK_TERM})
+def reply_tweets():
+    stream = api.request('statuses/filter', {'follow': TRACK_TERM})
 
-    for item in r:
-        print(item['text'] if 'text' in item else item)
+    for tweet in stream:
+        print(tweet['text'] if 'text' in tweet else tweet)
 
-        print(item['user'])
-
-        print(item['user']['id'])
-
-        if item['user']['id_str'] == TRACK_TERM :
-            if 'id' in item:
-                tweet_id = item['id']
-
-            print(item['text'] if 'text' in item else item)
-            t = api.request('statuses/update', {'status': TWEET_TEXT, 'in_reply_to_status_id': tweet_id})
-            print('SUCCESS' if t.status_code == 200 else 'PROBLEM: ' + t.text)
+        if is_tweet_valid(tweet):
+            print(tweet['text'] if 'text' in tweet else tweet)
+            reply(tweet['id'])
 
 def fav_tweets():
     stream = api.request('statuses/filter', {'follow': TRACK_TERM})
@@ -52,12 +42,13 @@ def is_tweet_valid(tweet):
 def tweeted_by_stalked_account(user_id):
     return (user_id == TRACK_TERM)
 
+def reply(tweet_id):
+    response = api.request('statuses/update', {'status': TWEET_TEXT, 'in_reply_to_status_id': tweet_id})
+    print('SUCCESS' if response.status_code == 200 else 'PROBLEM: ' + response.text)
 
 def fav(tweet_id):
     response = api.request('favorites/create', {'id': tweet_id})
     print('SUCCESS' if response.status_code == 200 else 'PROBLEM: ' + response.text)
-
-
 
 if __name__ == "__main__":
     fav_tweets()
