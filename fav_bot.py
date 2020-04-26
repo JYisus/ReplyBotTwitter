aacfa8e4feeb1fb0864d549dfacb0f67b1c1ac2e
Juan Jesús Padrón Hernández <jpadron@opencanarias.es>
2020-04-26 17:04:58 +0100

    Refactor general - creando la clase Twitter_bot

--- fav_bot.py
+++ fav_bot.py
@@ -7,48 +7,58 @@ API_KEY = environ['API_KEY']
 API_KEY_SECRET = environ['API_KEY_SECRET']
 ACCESS_TOKEN = environ['ACCESS_TOKEN']
 ACCESS_TOKEN_SECRET = environ['ACCESS_TOKEN_SECRET']
-
-TRACK_TERM =  '1705276416'
+STALKED_ACCOUNT = environ['STALKED_ACCOUNT'] # '1705276416'
 TWEET_TEXT = " Esto es una prueba"
-api = TwitterAPI(API_KEY, 
-                    API_KEY_SECRET,
-                    ACCESS_TOKEN,
-                    ACCESS_TOKEN_SECRET)
 
-def reply_tweets():
-    stream = api.request('statuses/filter', {'follow': TRACK_TERM})
+class TwitterBot:
+    def __init__(self, credentials):
+        self.api = TwitterAPI(credentials['api_key'], 
+                    credentials['api_key_secret'],
+                    credentials['access_token'],
+                    credentials['access_token_secret'])
 
-    for tweet in stream:
-        print(tweet['text'] if 'text' in tweet else tweet)
+    def reply_tweets(self):
+        stream = self.api.request('statuses/filter', {'follow': STALKED_ACCOUNT})
 
-        if is_tweet_valid(tweet):
+        for tweet in stream:
             print(tweet['text'] if 'text' in tweet else tweet)
-            reply(tweet['id'])
 
-def fav_tweets():
-    stream = api.request('statuses/filter', {'follow': TRACK_TERM})
+            if self.__is_tweet_valid(tweet):
+                print(tweet['text'] if 'text' in tweet else tweet)
+                self.__reply(tweet['id'])
 
-    for tweet in stream:
-        print(tweet['text'] if 'text' in tweet else tweet)
+    def fav_tweets(self):
+        stream = self.api.request('statuses/filter', {'follow': STALKED_ACCOUNT})
 
-        if is_tweet_valid(tweet):
+        for tweet in stream:
             print(tweet['text'] if 'text' in tweet else tweet)
-            fav(tweet['id'])
-            
 
-def is_tweet_valid(tweet):
-    return (('user' in tweet) &  tweeted_by_stalked_account(tweet['user']['id_str']) & ('id' in tweet))
+            if self.__is_tweet_valid(tweet):
+                print(tweet['text'] if 'text' in tweet else tweet)
+                self.__fav(tweet['id'])
+                
 
-def tweeted_by_stalked_account(user_id):
-    return (user_id == TRACK_TERM)
+    def __is_tweet_valid(self, tweet):
+        return (('user' in tweet) &  self.__tweeted_by_stalked_account(tweet['user']['id_str']) & ('id' in tweet))
 
-def reply(tweet_id):
-    response = api.request('statuses/update', {'status': TWEET_TEXT, 'in_reply_to_status_id': tweet_id})
-    print('SUCCESS' if response.status_code == 200 else 'PROBLEM: ' + response.text)
+    def __tweeted_by_stalked_account(self, user_id):
+        return (user_id == STALKED_ACCOUNT)
 
-def fav(tweet_id):
-    response = api.request('favorites/create', {'id': tweet_id})
-    print('SUCCESS' if response.status_code == 200 else 'PROBLEM: ' + response.text)
+    def __reply(self, tweet_id):
+        response = self.api.request('statuses/update', {'status': TWEET_TEXT, 'in_reply_to_status_id': tweet_id})
+        print('SUCCESS' if response.status_code == 200 else 'PROBLEM: ' + response.text)
+
+    def __fav(self, tweet_id):
+        response = self.api.request('favorites/create', {'id': tweet_id})
+        print('SUCCESS' if response.status_code == 200 else 'PROBLEM: ' + response.text)
 
 if __name__ == "__main__":
-    fav_tweets()
+    credentials = {
+        'api_key': 'API_KEY',
+        'api_key_secret':'API_KEY_SECRET',
+        'access_token':'ACCESS_TOKEN',
+        'access_token_secret':'ACCESS_TOKEN_SECRET'
+    }
+
+    bot = TwitterBot(credentials)
+    bot.fav_tweets()
